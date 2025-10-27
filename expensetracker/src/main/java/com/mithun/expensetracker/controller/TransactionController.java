2db5c4d60d3308ce40fb33f25e426bfa12ff896c
Kumar R <2375265@cognizant.com>
2025-10-27 17:18:13 +0530

    added controller code

--- expensetracker/src/main/java/com/mithun/expensetracker/controller/TransactionController.java
+++ expensetracker/src/main/java/com/mithun/expensetracker/controller/TransactionController.java
@@ -1,4 +1,44 @@
 package com.mithun.expensetracker.controller;
 
+import com.mithun.expensetracker.entity.User;
+import com.mithun.expensetracker.entity.UserTransaction;
+import com.mithun.expensetracker.service.TransactionService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Transaction;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.service.annotation.GetExchange;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("transaction")
+@RequiredArgsConstructor
 public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    @PostMapping("add")
+    public ResponseEntity<UserTransaction> makeTransaction(@RequestBody @Valid UserTransaction transaction, @AuthenticationPrincipal User user){
+        try {
+            return new ResponseEntity<>(transactionService.makeTransaction(transaction, user), HttpStatus.OK);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("getAllTransaction")
+    public ResponseEntity<List<UserTransaction>> getAllTransactions(@AuthenticationPrincipal User user){
+        System.out.println(user.getUsername());
+        return new ResponseEntity<>(transactionService.getAllTransactions(),HttpStatus.OK);
+    }
+
+    @GetMapping("getUserTransaction")
+    public ResponseEntity<List<UserTransaction>> getUserTransaction(@AuthenticationPrincipal User user){
+        return new ResponseEntity<>(transactionService.getUserTransaction(user.getUserId()),HttpStatus.OK);
+    }
+
 }
